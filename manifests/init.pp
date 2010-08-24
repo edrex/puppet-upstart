6d0b812de33e35671a41fa691b5d8d52f86e70fc
Eric Drechsel <eric@sqrly.com>
2010-08-24 17:43:09 +0000

    in prog

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,12 @@
 class upstart {
-    # git clone git://github.com/edavis10/redmine.git
-    # git checkout -b remotes/origin/0.9-stable
-    # /var/www/redmine (or /srv/redmine)
     package { upstart:
         ensure => installed,
     }
 }
 
-define upstart::job($description = '', $fork=false, $exec = null, $script = null) {
-    file { "/etc/init/upstart-${title}.conf":
+define upstart::job($description = '', $fork=false, $cmd='', $use_script=false) {
+    file { "/etc/init/${title}.conf":
         content => template('upstart/job.conf.erb'),
-	ensure => present
+        ensure => present
     }
 }
